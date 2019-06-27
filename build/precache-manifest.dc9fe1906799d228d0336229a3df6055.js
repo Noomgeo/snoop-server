6ae35760bfe3e8174135f5eae6d8eeabcdf3c4e8
noomgeo <noammizrahi@hotmail.com>
2019-06-27 14:48:48 +0300

    Started server

---
+++ build/precache-manifest.dc9fe1906799d228d0336229a3df6055.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef55edb32c2d84e190f2f75a9ffc4597",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4608eba1fe94e21dd012",
+    "url": "/static/css/main.ab864a69.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "4608eba1fe94e21dd012",
+    "url": "/static/js/main.4322ac4d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a2018c34d7e9616fe4548c7a4b334f6",
+    "url": "/static/media/goldRecord.6a2018c3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
